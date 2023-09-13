62e735d1f301cc973ccc46b32dbb92799da345fe
nnadiatull <144761054+nnadiatull@users.noreply.github.com>
2023-09-13 15:04:02 +0700

    Add files via upload

---
+++ mvc_nadia/model.php
@@ -0,0 +1,10 @@
+<?php
+class Model{
+    public function show_message(){
+        return array (
+            'title'=> 'Aplikasi MVC',
+            'body' => 'Halo Selamat Belajar Konsep MVC'
+        );
+    }
+}
+?>
---
+++ mvc_nadia/view.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+<title><?php echo $data['title'];?></title>
+<body>
+<h3><?php echo $data ['body'];?></h3>
+</body>
+</html>
